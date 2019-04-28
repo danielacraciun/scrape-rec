fbc11397f989655036813a27fde9cc6901f34ab5
mirceachira <chira.mircea.mc@gmail.com>
2019-04-28 11:24:38 +0300

    Added duplicate filtering and monitors.

--- scrape_rec/settings.py
+++ scrape_rec/settings.py
@@ -12,3 +12,23 @@ ITEM_PIPELINES = {
     'scrapy_jsonschema.JsonSchemaValidatePipeline': 300,
     'scrape_rec.pipelines.PostgresPipeline': 400
 }
+
+# Spidermon - monitoring tool
+SPIDERMON_ENABLED = True
+SPIDERMON_SPIDER_CLOSE_MONITORS = (
+    'scrape_rec.monitors.SpiderCloseMonitorSuite',
+)
+EXTENSIONS = {
+    'spidermon.contrib.scrapy.extensions.Spidermon': 500,
+}
+
+# Caching - cache responses on disk to save tons of time
+# Only cache product pages, not listing pages to ensure we get to all products!
+HTTPCACHE_ENABLED = True
+
+# Make sure this is set to the same value as the
+# docker-compose volume 'httpcache' for persistance
+HTTPCACHE_DIR = '/var/lib/httpcache/'
+
+# Save some space
+HTTPCACHE_GZIP = True
