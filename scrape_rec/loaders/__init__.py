7e9f4ee97614f950e8839fd6d562aea8c987a8b2
Daniela Craciun <craciundaniela19@gmail.com>
2019-10-05 10:24:13 +0200

    Removed additional scrapers

--- scrape_rec/loaders/__init__.py
+++ scrape_rec/loaders/__init__.py
@@ -1,6 +1,2 @@
 from .base_realestate_loader import BaseRealEstateAdLoader
-from .imobiliare_loader import ImobiliareAdLoader
-from .lajumate_loader import LajumateAdLoader
-from .olx_loader import OlxAdLoader
-from .storia_loader import StoriaAdLoader
 from .piata_az_loader import PiataAZAdLoader
